b4316feadf340c2501fbfb78a8ff42524e55cea8
cin-derella <shirley_cinderella@outlook.com>
2020-01-28 14:45:13 +0800

    place list and provider setup

---
+++ lib/providers/great_places.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/widgets.dart';
+import '../models/place.dart';
+
+class GreatPlaces with ChangeNotifier {
+  List<Place> _items = [];
+
+  List<Place> get items {
+    return [..._items];
+  }
+  
+
+}
