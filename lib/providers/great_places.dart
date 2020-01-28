4bb70e6590d031f53d7317d8fbdc4e49ba335133
cin-derella <shirley_cinderella@outlook.com>
2020-01-28 17:15:30 +0800

    manage data and image via provider

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,6 +1,6 @@
-
 import 'package:flutter/widgets.dart';
 import '../models/place.dart';
+import 'dart:io';
 
 class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
@@ -8,6 +8,18 @@ class GreatPlaces with ChangeNotifier {
   List<Place> get items {
     return [..._items];
   }
-  
 
+  void addPlace(
+    String pickedTitle,
+    File pickedImage,
+  ) {
+    final newPlace = Place(
+      id: DateTime.now().toString(),
+      image: pickedImage,
+      title: pickedTitle,
+      location: null,
+    );
+    _items.add(newPlace);
+    notifyListeners();
+  }
 }
